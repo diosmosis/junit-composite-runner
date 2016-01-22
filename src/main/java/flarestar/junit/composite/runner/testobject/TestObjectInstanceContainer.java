257599e489b6013ede2a0a718490e4745a0085d4
diosmosis <benaka@piwik.pro>
2016-01-22 12:44:20 -0800

    Use ReflectiveCallable in TestObjectInstanceContainer.setCurrentInstance().

--- src/main/java/flarestar/junit/composite/runner/testobject/TestObjectInstanceContainer.java
+++ src/main/java/flarestar/junit/composite/runner/testobject/TestObjectInstanceContainer.java
@@ -1,16 +1,23 @@
 package flarestar.junit.composite.runner.testobject;
 
+import org.junit.internal.runners.model.ReflectiveCallable;
+
 /**
  * TODO
  */
 public class TestObjectInstanceContainer {
     public static Object currentTestInstance = null;
 
-    public static void setCurrentInstance(Class<?> testObjectClass) {
+    public static void setCurrentInstance(final Class<?> testObjectClass) {
         try {
-            currentTestInstance = testObjectClass.newInstance();
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException(e);
+            currentTestInstance = new ReflectiveCallable() {
+                @Override
+                protected Object runReflectiveCall() throws Throwable {
+                    return testObjectClass.newInstance();
+                }
+            }.run();
+        } catch (Throwable throwable) {
+            throw new RuntimeException(throwable);
         }
     }
 }
