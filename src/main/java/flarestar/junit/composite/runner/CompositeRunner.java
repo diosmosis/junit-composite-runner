17429526a7e6df83bcb422fddd12198e052cda80
Benaka Moorthi <benaka.moorthi@mheducation.com>
2016-12-31 17:35:47 -0800

    Changes to pom.xml for release.

--- src/main/java/flarestar/junit/composite/runner/CompositeRunner.java
+++ src/main/java/flarestar/junit/composite/runner/CompositeRunner.java
@@ -19,36 +19,6 @@ import java.util.List;
 
 /**
  * TODO
- *
- * ok, so how do we implement this? we need to make sure run() nests run calls for each runner
- *
- * normal runner workflow is this:
- *   run classBlock() (runs before/after class stuff)
- *   run runChildren() (nested in classBlock())
- *   run runChild() (which will deal w/ test rules & whatever else)
- *   run runLeaf() (w/ actual statement) although mine doesn't do this all the time...
- *
- * how would we compose bdd runner + robolectric?
- *  -> we'd run run() on the first one (BddRunner)
- *     -> this will run the classBlock() on the first one which should eventually call runChildren()
- *  -> at this point, we don't want to run the child tests, we want to run the next runner's classBlock()
- *     -> the next runner's classBlock will call runChildren()
- *  -> at this point we want to move on to runChildren().
- *     -> what if both runners have different child tests? eg, what if BDD creates some methods, while the other one has other methods,
- *        how do I run all of them? and do I run some w/ one runner & some w/ another?
- *
- *        can't do this.
- *     -> runChildren() will only use the first runner
- *  -> the first runner's runChildren() will loop through each child and invoke runChild()
- *  -> runChild() will invoke runLeaf() for each leaf
- *  -> runLeaf() will invoke the statement which will eventually run the FrameworkMethod.
- *     * we need to create a proxy FrameworkMethod that will chain to the next runner's runChild()
- *  -> at the end, the proxy FrameworkMethod will just invoke the composited ParentRunner's runLeaf()
- *
- * so our TODO is:
- * - how do we ensure only one object is created per instance?
- *
- * TODO: create issue on junit issue tracker about this
  */
 public class CompositeRunner extends Runner {
 
